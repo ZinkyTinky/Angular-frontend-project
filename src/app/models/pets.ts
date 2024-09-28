2774bf251f03e753513d122453bfa439a74b29f7
Zian Jacobsen <25855271@sun.ac.za>
2024-09-28 21:35:52 +0200

    Finished models and created JSON API

---
+++ src/app/models/pets.ts
@@ -0,0 +1,8 @@
+export class Pets {
+        id: number = 0;
+        Name: string = '';
+        imageUrl: string = '';
+        price: number = 0;
+        description: string = '';
+        petType: string = '';
+}
