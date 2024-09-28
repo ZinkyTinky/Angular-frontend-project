8c3b365878d59ad80a6b287775e8ee89fd9c3348
Zian Jacobsen <25855271@sun.ac.za>
2024-09-28 23:03:12 +0200

    Added ngIf to show description for pet cards

--- src/app/components/pet-card/pet-card.component.ts
+++ src/app/components/pet-card/pet-card.component.ts
@@ -11,6 +11,9 @@ export class PetCardComponent implements OnInit {
     @Input()
     pet: Pets = new Pets();
 
+    @Input()
+    showPetDescription!: boolean;
+
     constructor() { }
 
     ngOnInit(): void {
